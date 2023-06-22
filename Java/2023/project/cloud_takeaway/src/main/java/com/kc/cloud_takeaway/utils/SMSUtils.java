6721a1541654706655ebd6659e8eeaa9daa28c76
929KC <3076675259@qq.com>
2023-06-22 23:47:45 +0800

    清空

--- Java/2023/project/cloud_takeaway/src/main/java/com/kc/cloud_takeaway/utils/SMSUtils.java
+++ Java/2023/project/cloud_takeaway/src/main/java/com/kc/cloud_takeaway/utils/SMSUtils.java
@@ -7,20 +7,12 @@ import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.profile.DefaultProfile;
 
-/**
- * 短信发送工具类
- */
+
 public class SMSUtils {
 
-	/**
-	 * 发送短信
-	 * @param signName 签名
-	 * @param templateCode 模板
-	 * @param phoneNumbers 手机号
-	 * @param param 参数
-	 */
+
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tBP6FzCVdZA8Xs3VHzz", "Y5msWsP0z3oOtZAAhxFmejX72t8NN4");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
