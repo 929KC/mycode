3b4dcd0f68f03cbd09cb03ad6392c474d6eb8c02
929KC <3076675259@qq.com>
2023-06-25 23:46:14 +0800

    内联函数

--- C++/2023/june/Project1/demo.cpp
+++ C++/2023/june/Project1/demo.cpp
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<iostream>
 #include"demo1.h"
+#include <time.h>
 using namespace std;
 //int rand = 10;
 //int main()
@@ -269,4 +270,101 @@ using namespace std;
 //	return 0;
 //}
 
+//传值和传引用的效率比较
 
+//struct A { int a[10000]; };
+//void TestFunc1(A a) {}
+//void TestFunc2(A& a) {}
+//void TestRefAndValue()
+//{
+//	A a;
+//	// 以值作为函数参数
+//	size_t begin1 = clock();
+//	for (size_t i = 0; i < 10000; ++i)
+//		TestFunc1(a);
+//	size_t end1 = clock();
+//	// 以引用作为函数参数
+//	size_t begin2 = clock();
+//	for (size_t i = 0; i < 10000; ++i)
+//		TestFunc2(a);
+//	size_t end2 = clock();
+//	// 分别计算两个函数运行结束后的时间
+//	cout << "TestFunc1(A)-time:" << end1 - begin1 << endl;
+//	cout << "TestFunc2(A&)-time:" << end2 - begin2 << endl;
+//}
+//
+//int main()
+//{
+//	
+//	TestRefAndValue();
+//	return 0;
+//}
+
+
+// 值和引用的作为返回值类型的性能比较
+struct A { int a[10000]; };
+A a;
+// 值返回
+//A TestFunc1() { return a; }
+//// 引用返回
+//A& TestFunc2() { return a; }
+//void TestReturnByRefOrValue()
+//{
+//	// 以值作为函数的返回值类型
+//	size_t begin1 = clock();
+//	for (size_t i = 0; i < 100000; ++i)
+//		TestFunc1();
+//	size_t end1 = clock();
+//	// 以引用作为函数的返回值类型
+//	size_t begin2 = clock();
+//	for (size_t i = 0; i < 100000; ++i)
+//		TestFunc2();
+//	size_t end2 = clock();
+//	// 计算两个函数运算完成之后的时间
+//	cout << "TestFunc1 time:" << end1 - begin1 << endl;
+//	cout << "TestFunc2 time:" << end2 - begin2 << endl;
+//}
+//
+//int main()
+//{
+//	TestReturnByRefOrValue();
+//	return 0;
+//}
+
+//6 引用和指针的区别
+//引用和指针的不同点:
+//1. 引用概念上定义一个变量的别名，指针存储一个变量地址。
+//2. 引用在定义时必须初始化，指针没有要求
+//3. 引用在初始化时引用一个实体后，就不能再引用其他实体，而指针可以在任何时候指向任何一个同类型
+//实体
+//4. 没有NULL引用，但有NULL指针
+//5. 在sizeof中含义不同：引用结果为引用类型的大小，但指针始终是地址空间所占字节个数(32位平台下占
+//	4个字节)
+//	6. 引用自加即引用的实体增加1，指针自加即指针向后偏移一个类型的大小
+//	7. 有多级指针，但是没有多级引用
+//	8. 访问实体方式不同，指针需要显式解引用，引用编译器自己处理
+//	9. 引用比指针使用起来相对更安全
+
+//int main()
+//{
+//	int a = 10;
+//
+//	int& ra = a;
+//	ra = 20;
+//
+//	int* pa = &a;
+//	*pa = 20;
+//	cout << "";
+//	return 0;
+//}
+
+
+ inline int Add(int left, int right)
+{
+	return left + right;
+}
+int main()
+{
+	int ret = Add(12, 23);
+	return 0;
+}
