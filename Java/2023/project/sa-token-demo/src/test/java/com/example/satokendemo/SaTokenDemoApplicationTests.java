d0ca8216cbc665cdb853465bd9d270b991cfea0d
929KC <3076675259@qq.com>
2023-07-06 19:19:35 +0800

    sa-token-demo1

---
+++ Java/2023/project/sa-token-demo/src/test/java/com/example/satokendemo/SaTokenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.satokendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaTokenDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
