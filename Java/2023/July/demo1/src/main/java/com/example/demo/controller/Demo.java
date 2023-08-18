3049992d8e16753c17b1b053461a9f6ddf835e4b
929KC <92706413+929KC@users.noreply.github.com>
2023-08-18 23:04:53 +0800

    idea远程部署-远程调试

---
+++ Java/2023/July/demo1/src/main/java/com/example/demo/controller/Demo.java
@@ -0,0 +1,7 @@
+package com.example.demo.controller;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java/2023/July/demo1/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloController {
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello World";
+    }
+}
