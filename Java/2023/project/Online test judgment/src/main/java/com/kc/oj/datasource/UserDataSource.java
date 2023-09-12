3700c8fcd2067b95f333707656d35d3b299356bd
929KC <92706413+929KC@users.noreply.github.com>
2023-09-12 23:14:06 +0800

    初始化后端-1

---
+++ Java/2023/project/Online test judgment/src/main/java/com/kc/oj/datasource/UserDataSource.java
@@ -0,0 +1,35 @@
+package com.kc.oj.datasource;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.kc.oj.model.dto.user.UserQueryRequest;
+import com.kc.oj.model.vo.UserVO;
+import com.kc.oj.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 用户服务实现
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Service
+@Slf4j
+public class UserDataSource implements DataSource<UserVO> {
+
+    @Resource
+    private UserService userService;
+
+    @Override
+    public Page<UserVO> doSearch(String searchText, long pageNum, long pageSize) {
+        UserQueryRequest userQueryRequest = new UserQueryRequest();
+        userQueryRequest.setUserName(searchText);
+        userQueryRequest.setCurrent(pageNum);
+        userQueryRequest.setPageSize(pageSize);
+        Page<UserVO> userVOPage = userService.listUserVoByPage(userQueryRequest);
+        return userVOPage;
+    }
+}
